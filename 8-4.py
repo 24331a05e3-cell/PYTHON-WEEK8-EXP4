98d29c1549406d3df06a9b4312a6c652d6a2c331
24331a05e3-cell <24331a05e3@mvgrce.edu.in>
2026-02-27 07:25:43 +0530

    Add files via upload

---
+++ 8-4.py
@@ -0,0 +1,7 @@
+#24331A05E3
+# add a key value pair to a dictionary and update it
+dict = {}
+dict.update(names=['BALA','ADITYA'],rolls=['1','2'])
+print(dict)
+dict.update(names=['KUSHAL','JASU'])
+print(dict)
